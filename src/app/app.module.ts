1b38e25c16e9042e8d3480560bd02a949e7f3fab
rehanhanar7 <rehanhanar7@gmail.com>
2021-10-02 13:20:05 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { MaterialModule } from '../angular-material/angular-material.module';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, MaterialModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
